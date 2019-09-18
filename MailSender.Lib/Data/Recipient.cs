ece4e1f8dc34e979bc31c93b0ebd30af74ce1751
Dragon1983@yandex.ru <Dragon1983@yandex.ru>
2019-09-18 18:47:07 +0300

    Урок 1. Структура приложения.

---
+++ MailSender.Lib/Data/Recipient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender.Lib.Data
+{
+    class Recipient
+    {
+        public string Name { get; set; }
+
+        public string Adress { get; set; }
+    }
+}
---
+++ MailSender.Lib/Data/Server.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender.Lib.Data
+{
+    class Server
+    {
+        public string Name { get; set; }
+
+        public string Adress { get; set; }
+
+        public int Port { get; set; }
+
+        public bool UseSSL { get; set; } = true;
+
+        public string UserName { get; set; }
+
+        public SecureString Password { get; set; }
+    }
+}
