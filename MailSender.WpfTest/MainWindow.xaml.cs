dec332e78db4a3739b274db784082d5cf1f05109
Dragon1983@yandex.ru <Dragon1983@yandex.ru>
2019-10-07 16:40:01 +0300

    Тестовый пример MVVM

--- MailSender.WpfTest/MainWindow.xaml.cs
+++ MailSender.WpfTest/MainWindow.xaml.cs
@@ -24,5 +24,7 @@ namespace MailSender.WpfTest
         {
             InitializeComponent();
         }
+
+        
     }
 }
---
+++ MailSender.WpfTest/MainWindowViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+using MailSender.Lib.MVVM;
+
+namespace MailSender.WpfTest
+{
+    class MainWindowViewModel : ViewModel
+    {
+        private string _Title = "TestApplication";
+
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        private double _Position;
+
+        public double Position
+        {
+            get => _Position;
+            set => Set(ref _Position, value);
+        }
+
+        private double _Angle;
+
+        public double Angle
+        {
+            get => _Angle;
+            set => Set(ref _Angle, value);
+        }
+
+        private DateTime _Time = DateTime.Now;
+
+        public DateTime Time
+        {
+            get => _Time;
+            set => Set(ref _Time, value);
+        }
+
+
+        private readonly DispatcherTimer _Timer;
+
+        public MainWindowViewModel()
+        {
+            _Timer = new DispatcherTimer()
+            {
+                Interval = TimeSpan.FromSeconds(0.2),
+                IsEnabled = true
+            };
+
+            _Timer.Tick += (s, e) => Time = DateTime.Now;
+        }
+    }
+}
