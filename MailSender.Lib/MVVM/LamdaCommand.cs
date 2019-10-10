6a7e5d6f926b0ba7b632ed896686e24fc6d97c86
Dragon1983@yandex.ru <Dragon1983@yandex.ru>
2019-10-10 18:16:01 +0300

    Create my own command.

---
+++ MailSender.Lib/MVVM/LamdaCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MailSender.Lib.MVVM
+{
+    public class LamdaCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        private readonly Action<object> _actionCommand;
+
+        private readonly Func<object, bool> _canExecute;
+
+        public LamdaCommand(Action<object> actionCommand, Func<object, bool> canExecute = null)
+        {
+            _actionCommand = actionCommand ?? throw new ArgumentNullException(nameof(actionCommand));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter) => _canExecute?.Invoke(parameter) ?? true;
+
+        public void Execute(object parameter) => _actionCommand(parameter);
+    }
+}
---
+++ MailSender/Commands/SaveDataWithMesageCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using MailSender.Lib.MVVM;
+
+namespace MailSender.Commands
+{
+    public class SaveDataWithMesageCommand : LamdaCommand
+    {
+        public SaveDataWithMesageCommand() : base(p => MessageBox.Show("Сохраняем"), p => p != null) { }
+       
+    }
+}
