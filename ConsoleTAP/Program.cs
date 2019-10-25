1a0911350d2814c7c0aeb3f53063d3269cf7fd1d
Dragon1983@yandex.ru <Dragon1983@yandex.ru>
2019-10-25 20:03:50 +0300

    Использование асинхронных методов в WPF.

--- ConsoleTAP/Program.cs
+++ ConsoleTAP/Program.cs
@@ -1,21 +1,24 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace ConsoleTAP
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             #region Параллельное программирование
 
-            //PrintInfo("Основной поток начат");
+            //PrintInfoAsync("Основной поток начат");
 
-            ////var task1 = Task.Run(() => PrintInfo("task1"));
+            ////var task1 = Task.Run(() => PrintInfoAsync("task1"));
 
             ////task1.Wait();
 
@@ -23,13 +26,13 @@ namespace ConsoleTAP
             //{
             //    //Thread.Sleep(1000);
             //    //Console.WriteLine($"№ {o: 3}, task {Task.CurrentId: 3}, Thread {Thread.CurrentThread.ManagedThreadId: 3}");
-            //    PrintInfo($"№ {o :D2}");
+            //    PrintInfoAsync($"№ {o :D2}");
             //})).ToArray();
 
             //Task t2 = Task.WhenAny(tasks);
             //Task<int> t3 = t2.ContinueWith(t =>
             //{
-            //    PrintInfo($"Статус {t.Status}");
+            //    PrintInfoAsync($"Статус {t.Status}");
             //    return Task.CurrentId ?? 0;
             //});
 
@@ -37,71 +40,166 @@ namespace ConsoleTAP
 
             //var t4 = factory.StartNew(p =>
             //{
-            //    PrintInfo($"Через фабрику, задача {(p as Task)?.Id}, статус {(p as Task)?.Status}");
+            //    PrintInfoAsync($"Через фабрику, задача {(p as Task)?.Id}, статус {(p as Task)?.Status}");
             //    return Task.CurrentId ?? 0;
             //}, t2, TaskCreationOptions.LongRunning);
 
-            //t4.ContinueWith(t => PrintInfo(t.Result.ToString()));
+            //t4.ContinueWith(t => PrintInfoAsync(t.Result.ToString()));
 
-            //PrintInfo($"Закончено выполнение задачи {t3.Result}");
+            //PrintInfoAsync($"Закончено выполнение задачи {t3.Result}");
 
-            //PrintInfo("Основной поток завершён");
+            //PrintInfoAsync("Основной поток завершён");
 
             //Console.WriteLine("\n\nParallel");
 
-            //Parallel.For(1, 11, (i, p) => PrintInfo($"i = {i}, loop = {p}"));
+            //Parallel.For(1, 11, (i, p) => PrintInfoAsync($"i = {i}, loop = {p}"));
 
-            //PrintInfo("Основной поток завершён");
+            //PrintInfoAsync("Основной поток завершён");
 
 
             //Console.WriteLine("\n\nParallelLINQ");
 
             //Enumerable.Range(1, 30).AsParallel().Select(i =>
             //{
-            //    PrintInfo(i.ToString());
+            //    PrintInfoAsync(i.ToString());
             //    return i;
             //}).ToArray();
 
-            //PrintInfo("Основной поток завершён");
+            //PrintInfoAsync("Основной поток завершён");
 
 
             #endregion
 
 
-            Console.WriteLine("\n\nAsync");
+            //Console.WriteLine("\n\nAsync");
+
+            //PrintInfoAsync("Основной поток начат");
+
+            //Func<string, int> func = PrintInfoIntAsync;
+
+            //func.Invoke("Invoke");
+
+            //func.BeginInvoke("BeginInvoke", ar => PrintInfoAsync($"EndInvoke result: {func.EndInvoke(ar)}"), null);
+
+            //var resAsync = func.BeginInvoke("BeginInvoke2", ar => PrintInfoAsync("BeginInvoke2 завершился"), null);
+
+            //PrintInfoAsync($"Текущий статус завершения BeginInvoke2: {resAsync.IsCompleted}");
+
+            //var result = func.EndInvoke(resAsync);
+
+            //PrintInfoAsync($"Получили результат EndInvoke2 в основном потоке: {result}");
+
+            //PrintInfoAsync("Основной поток завершён");
 
             PrintInfo("Основной поток начат");
 
-            Func<string, int> func = PrintInfoInt;
+            //IEnumerable<int> alexsInts = Enumerable.Range(1, 20);
+            //alexsInts.Print(nameof(alexsInts));
+            //var transformed = from x in alexsInts
+            //    where x != 9
+            //    select x + 2;
+            //transformed.Print(nameof(transformed));
 
-            func.Invoke("Invoke");
+            //ModifyData(alexsInts);
 
-            func.BeginInvoke("BeginInvoke", ar => PrintInfo($"EndInvoke result: {func.EndInvoke(ar)}"), null);
+            ConcoleOut();
 
-            var resAsync = func.BeginInvoke("BeginInvoke2", ar => PrintInfo("BeginInvoke2 завершился"), null);
+            for (int i = 0; i < 30; i++)
+            {
+                await Task.Delay(500);
+                //await Delay(500);
+                Console.WriteLine($"Что-то делаем {i}");
+            }
+            
 
-            PrintInfo($"Текущий статус завершения BeginInvoke2: {resAsync.IsCompleted}");
+                    Console.ReadLine();
+        }
 
-            var result = func.EndInvoke(resAsync);
+        private static Task Delay(int millisec)
+        {
+            TaskCompletionSource<object> tcs = new TaskCompletionSource<object>();
+            Timer timer = new Timer(t => tcs.SetResult(null), null, millisec, Timeout.Infinite);
+            tcs.Task.ContinueWith(t => timer.Dispose());
+            return tcs.Task;
+        }
 
-            PrintInfo($"Получили результат EndInvoke2 в основном потоке: {result}");
+        private static async void ConcoleOut()
+        {
+            if (await GetUserPremission())
+            {
+                Console.WriteLine("Получен положительный ответ");
+            }
+            else
+            {
+                Console.WriteLine("Получен отрицательный ответ");
+            }
+        }
 
-            PrintInfo("Основной поток завершён");
-
-            Console.ReadLine();
+        private static async Task PrintInfoAsync(string str = null)
+        {
+            Thread.Sleep(1000);
+            Console.WriteLine($"Задача: {Task.CurrentId :D2}, Поток {Thread.CurrentThread.ManagedThreadId :D2}, {str}");
         }
 
         private static void PrintInfo(string str = null)
         {
-            //Thread.Sleep(1000);
-            Console.WriteLine($"Задача: {Task.CurrentId :D2}, Поток {Thread.CurrentThread.ManagedThreadId :D2}, {str}");
-        }
-
-        private static int PrintInfoInt(string str = null)
-        {
             Thread.Sleep(1000);
             Console.WriteLine($"Задача: {Task.CurrentId:D2}, Поток {Thread.CurrentThread.ManagedThreadId:D2}, {str}");
-            return Task.CurrentId ?? 0;
+        }
+
+        private static async Task<int> PrintInfoIntAsync(string str = null)
+        {
+            Thread.Sleep(1000);
+            await PrintInfoAsync(str + " innerCall");
+            Console.WriteLine($"Задача: {Task.CurrentId:D2}, Поток {Thread.CurrentThread.ManagedThreadId:D2}, {str}");
+            return Task.CurrentId ?? 1;
+        }
+
+        private static async void ModifyData(IEnumerable<int> list)
+        {
+            IEnumerable<int> transformed;
+            IEnumerable<Task<int>> tasks = list
+                .Where(x => x != 9)
+                .AsParallel()
+                .Select(async x => await PrintInfoIntAsync($"{x} call 1") + await PrintInfoIntAsync($"{x} call 2"))
+                .ToArray();
+            transformed = await Task.WhenAll(tasks);
+            transformed.Print(nameof(transformed) + " await");
+        }
+
+        [STAThread]
+        public static Task<bool> GetUserPremission()
+        {
+            TaskCompletionSource<bool> tcs = new TaskCompletionSource<bool>();
+
+            var t1 = Task.Run(() =>
+                {
+                    PrintInfo("Dialog");
+                    return MessageBox.Show("Согласны?", "Вопрос", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                }
+                );
+            t1.ContinueWith(r =>
+            {
+                PrintInfo("Result");
+                tcs.SetResult(r.Result == MessageBoxResult.Yes);
+            });
+
+            PrintInfo("GetUserPremission");    
+            
+            return tcs.Task;
+        }
+    }
+
+    public static class Extentions
+    {
+        public static void Print<T>(this IEnumerable<T> list, string name = null)
+        {
+            Console.Write($"{name ?? list.GetType().Name}: count {list.Count()}, values: ");
+            foreach (var item in list)
+            {
+                Console.Write($"{item}, ");
+            }
+            Console.WriteLine();
         }
     }
 }
---
+++ WpfProgressTest/ViewModel/MainViewModel.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using MailSender.Lib.MVVM;
+
+namespace WpfProgressTest.ViewModel
+{
+    public class MainViewModel : MailSender.Lib.MVVM.ViewModel
+    {
+        private int _result;
+
+        public int Result
+        {
+            get => _result;
+            set => Set(ref _result, value);
+        }
+
+        private double _computeProgress;
+
+        public double ComputeProgress
+        {
+            get => _computeProgress;
+            set => Set(ref _computeProgress, value);
+        }
+
+
+        public ICommand ComputeSummCommand { get; }
+
+        private bool _canComputeSummExecute = true;
+
+        public ICommand CancelComputeSummCommand { get; }
+
+        public MainViewModel()
+        {
+            ComputeSummCommand = new LamdaCommand(OnComputeSummExecuteAsync, CanComputeSummExecute);
+            CancelComputeSummCommand = new LamdaCommand(OnCancelComputeSummExecuteAsync, CanCancelComputeSummExecute);
+        }
+
+        private void OnCancelComputeSummExecuteAsync(object obj) => _cancellationTokenSource?.Cancel();
+
+
+        private CancellationTokenSource _cancellationTokenSource;
+
+        private bool CanComputeSummExecute(object obj) => _canComputeSummExecute;
+
+        private bool CanCancelComputeSummExecute(object obj) => _cancellationTokenSource != null;
+
+        private async void OnComputeSummExecuteAsync(object obj)
+        {
+            _canComputeSummExecute = false;
+            ComputeProgress = 0;
+            _cancellationTokenSource = new CancellationTokenSource();
+            IProgress<(double, int)> progress = new Progress<(double, int)>(p =>
+            {
+                ComputeProgress += p.Item1;
+                Result = p.Item2;
+            });
+
+            Task<int> task = ComputeSummAsync(100, 500, _cancellationTokenSource.Token, progress);
+
+            try
+            {
+               Result = await task;
+            }
+            catch (AggregateException e)
+            {
+                foreach (Exception exception in e.InnerExceptions)
+                {
+                    if (exception is OperationCanceledException || exception?.InnerException is OperationCanceledException)
+                    {
+                        Result = 0;
+                        ComputeProgress = 0;
+                        //CommandManager.InvalidateRequerySuggested();
+                    }
+                    else
+                    {
+                        Task.Run(() => MessageBox.Show(exception.Message, "Error:"));
+                    }
+               }
+            }
+            _canComputeSummExecute = true;
+            _cancellationTokenSource = null;
+            CommandManager.InvalidateRequerySuggested();
+        }
+
+        private async Task<int> ComputeSummAsync(int end, int delay, CancellationToken cancellationToken = default,
+            IProgress<(double, int)> progress = null)
+        {
+            return await Task.Run(() => ComputeSumm(end, delay, cancellationToken, progress));
+        }
+
+        private static int ComputeSumm(int end, int delay, CancellationToken cancellationToken = default, IProgress<(double, int)> progress = null)
+        {
+            var result = 0;
+            //ParallelOptions parallelOptions = new ParallelOptions();
+            //parallelOptions.CancellationToken = cancellationToken;
+
+            Parallel.For(1,
+                end + 1,
+                (i, s) =>
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+                    Interlocked.Add(ref result, i);
+                    Task.Delay(delay, cancellationToken).Wait();
+                    progress?.Report(((double)1 / end, result));
+                });
+
+            //for (int i = 0; i <= end; i++)
+            //{
+            //    cancellationToken.ThrowIfCancellationRequested();
+            //    result += i;
+            //    Task.Delay(delay, cancellationToken).Wait();
+            //    progress?.Report(((double)i / end, result));
+            //}
+            //throw new ArgumentNullException("progress", "Finished");
+            return result;
+        }
+    }
+}
